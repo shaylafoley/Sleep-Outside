011ba3a459706bd7fd96efe6c6e146fa942da760
shaylafoley <wholemotherhood@gmail.com>
2024-09-26 21:34:12 -0400

    team activity week 2

--- src/js/product.js
+++ src/js/product.js
@@ -1,20 +1,26 @@
-import { setLocalStorage } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
+import { setLocalStorage, getParams } from "./utils.mjs";
+import ProductDetails from "./ProductDetails.mjs";
 
 const dataSource = new ProductData("tents");
-function addProductToCart(product) {
+const productId = getParams("product");
+
+const product = new ProductDetails(productId, dataSource);
+product.init();
+
+function addProductToCart(selectedProduct) {
   let cartItems = setLocalStorage("so-cart") || [];
-  
+
   // Ensure cartItems is always an array
   if (!Array.isArray(cartItems)) {
     cartItems = [cartItems];
   }
 
   // Add new product to cart
-  cartItems.push(product);
+  cartItems.push(selectedProduct);
 
   // Save back to local storage
-  setLocalStorage('so-cart', cartItems);
+  setLocalStorage("so-cart", cartItems);
 }
 
 //function addProductToCart(product) {
@@ -22,8 +28,8 @@ function addProductToCart(product) {
 //}
 // add to cart button event handler
 async function addToCartHandler(e) {
-  const product = await dataSource.findProductById(e.target.dataset.id);
-  addProductToCart(product);
+  const selectedProduct = await dataSource.findProductById(e.target.dataset.id);
+  addProductToCart(selectedProduct);
 }
 
 // add listener to Add to Cart button
