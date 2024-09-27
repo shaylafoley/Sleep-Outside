ff79ea66f44317d607127d7fc250d2c9d47bea0d
nelsonmonteiro85 <nelson-monteiro85@hotmail.com>
2024-09-27 12:10:26 +0100

    W01 Redone

--- src/js/product.js
+++ src/js/product.js
@@ -4,7 +4,7 @@ import ProductData from "./ProductData.mjs";
 const dataSource = new ProductData("tents");
 function addProductToCart(product) {
   let cartItems = setLocalStorage("so-cart") || [];
-  
+
   // Ensure cartItems is always an array
   if (!Array.isArray(cartItems)) {
     cartItems = [cartItems];
@@ -14,7 +14,7 @@ function addProductToCart(product) {
   cartItems.push(product);
 
   // Save back to local storage
-  setLocalStorage('so-cart', cartItems);
+  setLocalStorage("so-cart", cartItems);
 }
 
 //function addProductToCart(product) {
