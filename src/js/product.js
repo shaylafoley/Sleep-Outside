a39c4f44e838178569df675a6e266907aa922b46
Andre Dos Reis <andredosreis@gmail.com>
2024-09-29 04:46:40 +0100

    Atualizando mudanças no product.

--- src/js/product.js
+++ src/js/product.js
@@ -1,35 +1,35 @@
+import { setLocalStorage, getLocalStorage } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
-import { setLocalStorage, getParams } from "./utils.mjs";
-import ProductDetails from "./ProductDetails.mjs";
 
 const dataSource = new ProductData("tents");
-const productId = getParams("product");
 
-const product = new ProductDetails(productId, dataSource);
-product.init();
+// Função para adicionar o produto ao carrinho
+function addProductToCart(product) {
+  // Obtém o carrinho atual do localStorage, ou um array vazio se não houver produtos ainda
+  let cart = getLocalStorage("so-cart") || [];
 
-function addProductToCart(selectedProduct) {
-  let cartItems = setLocalStorage("so-cart") || [];
+  // Verifica se o produto já está no carrinho
+  const existingProduct = cart.find((item) => item.Id === product.Id);
 
-  // Ensure cartItems is always an array
-  if (!Array.isArray(cartItems)) {
-    cartItems = [cartItems];
+  if (existingProduct) {
+    // Se o produto já estiver no carrinho, incrementa a quantidade
+    existingProduct.quantity += 1;
+  } else {
+    // Se o produto ainda não estiver no carrinho, adiciona o novo produto
+    product.quantity = 1; // Inicializa a quantidade do produto no carrinho
+    cart.push(product);
   }
 
-  // Add new product to cart
-  cartItems.push(selectedProduct);
+  // Atualiza o localStorage com o novo carrinho
+  setLocalStorage("so-cart", cart);
+  alert(`${product.Name} add to card`);
 
-  // Save back to local storage
-  setLocalStorage("so-cart", cartItems);
 }
 
-//function addProductToCart(product) {
-//  setLocalStorage("so-cart", product);
-//}
-// add to cart button event handler
+// M// add to cart button event handler
 async function addToCartHandler(e) {
-  const selectedProduct = await dataSource.findProductById(e.target.dataset.id);
-  addProductToCart(selectedProduct);
+  const product = await dataSource.findProductById(e.target.dataset.id);
+  addProductToCart(product);
 }
 
 // add listener to Add to Cart button
