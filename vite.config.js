ecfa0ee8b475f5b2e05a1ac049cd52d755e1565b
shaylafoley <wholemotherhood@gmail.com>
2024-10-03 18:22:31 -0400

    vite config to fix build error

--- vite.config.js
+++ vite.config.js
@@ -14,7 +14,7 @@ export default defineConfig({
       input: {
         main: resolve(__dirname, "src/index.html"),
         cart: resolve(__dirname, "src/cart/index.html"),
-        product: resolve(__dirname, "src/product_pages/index.html"), // Updated to use the new product index
+        product: resolve(__dirname, "src/product_pages/index.html"),
       },
     },
   },
