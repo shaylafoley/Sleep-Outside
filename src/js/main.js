b43e6dc16f2dcdfddeb2f35099af1bd7303e2853
shaylafoley <wholemotherhood@gmail.com>
2024-10-03 14:16:03 -0400

    Dynamic header and footer

--- src/js/main.js
+++ src/js/main.js
@@ -1,20 +1,27 @@
+import { loadHeaderFooter } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
 import ProductListing from "./ProductList.mjs";
+
+loadHeaderFooter();
 
 const productData = new ProductData("tents");
 
 async function loadProducts() {
-    try {
-        const products = await productData.getProducts();
-        console.log(products); // Check if products are logged correctly
+  try {
+    const products = await productData.getProducts();
+    console.log(products); // Check if products are logged correctly
 
-        const listElement = document.querySelector(".product-list"); // Ensure this selector matches your HTML structure
+    const listElement = document.querySelector(".product-list"); // Ensure this selector matches your HTML structure
 
-        const productListing = new ProductListing("Tents", productData, listElement);
-        await productListing.init();
-    } catch (error) {
-        console.error("Error loading products:", error);
-    }
+    const productListing = new ProductListing(
+      "Tents",
+      productData,
+      listElement,
+    );
+    await productListing.init();
+  } catch (error) {
+    console.error("Error loading products:", error);
+  }
 }
 
 loadProducts();
